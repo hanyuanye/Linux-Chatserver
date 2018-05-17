5d0a0e45673035ae9b93acf51f1462f88dd47d0b
Hanyuan Ye <yehanyuan1999@gmail.com>
2018-05-16 22:07:58 -0400

    Server continously running

--- client.c
+++ client.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
 	int socketfd, port, code;
@@ -10,27 +11,30 @@ int main(int argc, char* argv[]) {
 	struct hostent* server;
 	char buffer[256];
 	if (argc < 3) {
+		printf("Not enough args");
 		return 0;	
 	}
 	
 	port = atoi(argv[2]);
-	socketfd = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (socketfd < 0) return 0;
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketfd < 0) {
+		printf("Error socket %d", errno);	
+		return 0;
+	}
 	server = gethostbyname(argv[1]);
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char*)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
 	serv_addr.sin_port = htons(port);
 	
-	if (connect(socketfd, &serv_addr, sizeof(serv_addr)) < 0) return 0;
+	if ((connect(socketfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr))) < 0) {
+		printf("Error connect %d", errno);
+		return 0;
+	}
 	
 	printf("Enter msg");
 	bzero(buffer, 256);
 	fgets(buffer, 255, stdin);
 	code = write(socketfd, buffer, 255);
 	if (code < 0) return 0;
-	bzero(buffer, 256);
-	code = read(socketfd, buffer, 255);
-	if (code < 0) return 0;
-	printf("s%", buffer);
 }
--- server.c
+++ server.c
@@ -1,42 +1,47 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <errno.h>
 #include <netinet/in.h>
 
+void respond(int socketfd) {
+	char buffer[256];
+	int code = read(socketfd, buffer, 255);	
+	printf("%s", buffer);
+	
+}
 
 int main(int argc, char *argv[]) {
-	int socketfd = (AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in serv_addr, peer_addr;
-	bzero((char *) &serv_addr, sizeof(serv_addr));
 	if (argc < 2) {
 		printf("Not enough arguments");
 		return 0;
 	}
+	int socketfd;
+	if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error %d", errno);
+		return 0;
+	}
+	struct sockaddr_in serv_addr, peer_addr;
+	bzero((char *) &serv_addr, sizeof(serv_addr));
 	int port = atoi(argv[1]);
-	serv_addr.sin_family = AF_UNIX;
+	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	
-	if (bind(socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		printf("Error on bind");
+	int bound = bind(socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if (bound < 0) {
+		printf("Error on bind %d", errno);
 		return 0;
 	}
 	listen(socketfd, 5);
 	int peerlen = sizeof(peer_addr);
-	int newSocketfd = accept(socketfd, (struct sockaddr *) &peer_addr, &peerlen);
-	if (newSocketfd < 0) {
-		printf("Error on accept");
-	}
-	char buffer[256];
-	bzero(buffer, 256);
-	int code = read(newSocketfd, buffer, 255);
-	if (code < 0) {
-		printf("Error reading");
-	}
-	printf("%s", buffer);
-	code = write(newSocketfd, "message", 7);
-	if (code < 0) {
-		printf("Error writing");	
+	while (1) {
+		int newSocketfd = accept(socketfd, (struct sockaddr *) &peer_addr, &peerlen);
+		int pid = fork();
+		if (pid == 0) {
+			close(socketfd);
+			respond(newSocketfd);
+			exit(0);
+		}
 	}
 }
 	
